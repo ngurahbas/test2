dc17a3782f384404be76b422f01572a1a522d3f8
ngurahbas <ngurahbaskara@gmail.com>
2026-02-24 14:54:12 +0800

    add Angular build integration and SpaController tracer code

---
+++ src/main/java/xs/test2/SpaController.java
@@ -0,0 +1,13 @@
+package xs.test2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SpaController {
+
+    @GetMapping(value = "/{path:[^\\.]*}")
+    public String forward() {
+        return "forward:/angular/index.html";
+    }
+}
