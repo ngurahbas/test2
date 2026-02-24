a6d77c10430027f96ef448e389e322301470cc65
ngurahbas <ngurahbaskara@gmail.com>
2026-02-24 20:18:44 +0800

    add test container

--- src/test/java/xs/test2/Test2ApplicationTests.java
+++ src/test/java/xs/test2/Test2ApplicationTests.java
@@ -1,13 +1,31 @@
 package xs.test2;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class Test2ApplicationTests {
 
+    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:18.2")
+        .withDatabaseName("testdb");
+
+    @BeforeAll
+    static void beforeAll() {
+        postgresContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
+
     @Test
     void contextLoads() {
     }
-
 }
