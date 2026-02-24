3def9847d1cfeb4c41c91f02cb753cfe7ae87318
ngurahbas <ngurahbaskara@gmail.com>
2026-02-24 22:06:36 +0800

    Simplify `AustralianAddress` by removing constructor and redundant annotations.

--- src/main/java/xs/test2/entity/AustralianAddress.java
+++ src/main/java/xs/test2/entity/AustralianAddress.java
@@ -1,34 +1,15 @@
 package xs.test2.entity;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class AustralianAddress {
 
-    @JsonCreator
-    public AustralianAddress(@JsonProperty("address") String address,
-                             @JsonProperty("suburb") String suburb,
-                             @JsonProperty("state") String state,
-                             @JsonProperty("postcode") String postcode) {
-        this.address = address;
-        this.suburb = suburb;
-        this.state = state;
-        this.postcode = postcode;
-    }
-
-    public AustralianAddress() {
-    }
-
-    @JsonProperty("address")
     private String address;
 
-    @JsonProperty("suburb")
     private String suburb;
 
-    @JsonProperty("state")
     private String state;
 
-    @JsonProperty("postcode")
     private String postcode;
 
     public String getAddress() {
